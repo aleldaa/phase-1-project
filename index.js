46d70afdb5a3596c19c1e1244b9a0fd4efc4e5a2
Luna Daniels <aleldaproductions@gmail.com>
2023-04-03 14:32:00 -0600

    added gitignore, also added ids for the li

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const title = document.querySelector('h1')
+require("dotenv").config()
+const express = require('express');
+const axios = require('axios')
+const app = express()
+const apiUrl = "https://imdb-api.com/en/API/Top250Movies/"
+
+app.get('/hello', (req,res)=> {
+    res.send("Hello Api")
+})
+app.get('/data', async(req,res,next)=>{
+    axios.request(apiUrl + process.env.API_KEY)
+    .then((response) => {
+        res.json(response.data)
+    })
+})
+app.listen(3000, ()=> {
+    console.log("started")
+})
+ 
+// const movieListContainer = document.querySelector('#movie-list-container')
+// const movieList = document.querySelector('#movie-list')
+// const movie1 = document.querySelector("#movie1")
+
+// movie1.src = data.image
