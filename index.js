7d3d4b1135b86629a0b436047a2fff109c2d4317
Sterling <sterlingbarton81@gmail.com>
2023-04-04 12:20:43 -0600

    added db.json for when needed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/data")
+fetch("http://localhost:3000/items")
 .then(response=>response.json())
 .then(response=>{
     console.log(response)
@@ -10,13 +10,16 @@ let global
 const movieList = document.querySelector('#movie-list-container')
 
 const getMovies = (movies) => {
-    for(let i = 0; i < 9; i++){
+    // for(let i = 0; i < 9; i++){
+        movies.forEach(movie => {
         let movieCard = document.createElement('li')
         movieCard.classList.add('col-4')
-        let url = movies.items[i].image
+        let url = movie.image
         let image = document.createElement('img')
         image.src = url
         movieCard.append(image)
         movieList.append(movieCard)
-    }
+        });
+        
+    // }
 }
