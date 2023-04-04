41c84883c74ae90bec5114b6bb8a33f0a789f45c
Luna Daniels <aleldaproductions@gmail.com>
2023-04-04 08:17:26 -0600

    small edits to html, added css file

--- backend.js
+++ backend.js
@@ -1,10 +1,9 @@
-// import express from 'express'
 require("dotenv").config()
 const express = require('express');
 const axios = require('axios')
 const app = express()
 const apiUrl = "https://imdb-api.com/en/API/Top250Movies/"
-// console.log(process.env.API_KEY)
+
 app.get('/hello', (req,res)=> {
     res.send("Hello Api")
 })
--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-// import axios from 'axios'
-// import express from 'express'
-// require("dotenv").config()
-// const express = require('express');
-// const axios = require('axios')
-// const app = express()
-// const apiUrl = "https://imdb-api.com/en/API/Top250Movies/"
-// console.log(process.env.API_KEY)
-// app.get('/hello', (req,res)=> {
-//     res.send("Hello Api")
-// })
-// app.get('/data', async(req,res,next)=>{
-//     const options = {
-//         method: "GET",
-//         url: `${apiUrl + process.env.API_KEY}`,
-//         headers: {
-//          "Content-Type": "application/json",
-//         },
-//        }
-//     axios.request(options)
-//     .then((response) => {
-//         res.json(response.data)
-//     })
-// })
-// app.listen(3000, ()=> {
-//     console.log("started")
-// })
-
 fetch("http://localhost:3000/data")
 .then(response=>response.json())
 .then(response=>console.log(response))
 .catch(err => console.log(err))
+.then(data => getMovies(data))
+
+let global
+const movie1 = document.querySelector('#movie1')
+
+const getMovies = (movies) => {
+    movies.forEach(content =>)
+}
