6c2acbb7701d69d5258acab42c714797a4a9da5b
Angela Palaszewski <delarosa.ang@gmail.com>
2023-04-04 14:45:12 -0600

    added click for details

--- index.js
+++ index.js
@@ -19,7 +19,27 @@ const getMovies = (movies) => {
         image.src = url
         movieCard.append(image)
         movieList.append(movieCard)
-        });
         
-    // }
+        
+        movieCard.addEventListener('click', () => {
+            let movieDetails = document.querySelector('#movie-details');
+            let movieTitle = document.createElement('p');
+            let releaseYear = document.createElement('p');
+            let movieCast = document.createElement('p');
+            let movieRating = document.createElement('p');
+            let movieImage = document.createElement('img');
+            
+            movieImage.src = movie.image
+            movieTitle.textContent = movie.title;
+            releaseYear.textContent = movie.year;
+            movieCast.textContent = `Main Cast: ${movie.crew}`;
+            movieRating.textContent = `imDb Rating: ${movie.imDbRating}`;
+            while(movieDetails.firstChild){
+                movieDetails.removeChild(movieDetails.lastChild);
+            }
+            movieDetails.append(movieImage, movieTitle, releaseYear, movieCast, movieRating);
+
+        })
+    });
 }
+
